6eb3237ad2e60ff5f4c4a8d60573ec1ba630e48b
Jean-Baptiste MAKAYA <jean-baptiste.makaya@externe.maif.fr>
2022-01-18 22:48:28 +0100

    merge main

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - PROD
+          Learn React
         </a>
       </header>
     </div>
