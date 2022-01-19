ad7f76f538d2b3946e5e065623be4081b3d49e84
Jean-Baptiste MAKAYA <jean-baptiste.makaya@externe.maif.fr>
2022-01-19 11:54:10 +0100

    livraison reecette

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Squad dev
+          Learn React - R7
         </a>
       </header>
     </div>
